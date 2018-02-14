b8f96b87e8325921423c9a663416dfff740a0725
VolodymyrTrykoz <mr.mastachok@gmail.com>
2018-02-14 23:18:21 +0200

    removed unnecessary scc styles

--- common.js
+++ common.js
@@ -1,85 +1,3 @@
-/*
-var dataArr = document.querySelectorAll('[data-modal]');
-var modal = document.querySelector('.modal-container');
-var modalSignIn = document.querySelector('[data-modal-body="sign_in"]');
-var modalSignUp = document.querySelector('[data-modal-body="sign_up"]');
-var close = document.querySelector('.menu-m__close');
-var wrapper = document.querySelector('.body__wrapper');
-
-for(i = 0; i < dataArr.length; i++){
-
-  dataArr[i].addEventListener('click', function(e){
-      e.preventDefault();
-      var link  = this;
-      var linkAttr = link.getAttribute('data-modal');
-      if (linkAttr == 'sign_in'){
-        modal.classList.remove('close');
-        modalSignIn.style.display = 'block';
-        
-        wrapper.classList.add('blur');
-      }
-
-      else {
-        modal.classList.remove('close');
-        modalSignUp.style.display = 'block';
-        wrapper.classList.add('blur');
-      }
-      
-  });
-}
-
-
-close.addEventListener('click', function(e){
-  e.preventDefault();
-  modal.classList.add('close');
-  modalSignIn.style.display = 'none';
-  modalSignUp.style.display = 'none';
-  wrapper.classList.remove('blur');
-})
-*/
-
-
-
-
-
-
-// window.onscroll = function() {
-//   var square = document.querySelector('.square');
-//   var squareOffset = square.offsetTop;
-//   var squareAttrOffset = square.getAttribute('data-offset');
-//   var newOffs = squareOffset + parseInt(squareAttrOffset);
-  
-//   if (window.scrollY > newOffs) {
-//     square.classList.add('squareChanged');
-//   }
-//   else {
-//     square.classList.remove('squareChanged');
-//   }
-// }
-
-// var scrollMagicMate = {
-   
-//     className: document.querySelector('.js-scrollmagic'),
-   
-//     init: function(){
-//       this.className.className;
-//     },
-//     addClass: function(){
-//      this.className.classList.add('squareChanged');
-//      console.log(this);
-//     },
-//   }
-
-// vovka = {
-//   className: 'vovka'
-// }
-//   scrollMagicMate.init(vovka);
-//   scrollMagicMate.addClass();
-
-
-
-
-  
 
 var product = document.querySelector('.product');
 var btn = document.querySelector('.btn');
@@ -102,9 +20,9 @@ btn.addEventListener('click', function(){
         }
       };
 
-      // request.onerror = function() {
-      //   // There was a connection error of some sort
-      // };
+      request.onerror = function() {
+        // There was a connection error of some sort
+      };
 
       request.send();
 
